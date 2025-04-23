a3725f5d87f8449ee2b16eba3714178562109a62
Shivangi541 <shivangidutta68@gmail.com>
2025-04-23 11:51:46 +0530

    added filter feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,25 @@
 import RestCard from "./RestCard";
 import resObj from "../utils/mockData";
+
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">
+      {/* <div className="search">
         <input type="text" placeholder="Search" />
         <button>Search</button>
+      </div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            resObj = resObj.filter(
+              (restaurant) => restaurant.info.avgRating > 4.3
+            );
+            console.log("Filtered Restaurants:", resObj);
+          }}
+        >
+          Top rated Restaurants
+        </button>
       </div>
       <div className="restaurant-list">
         {resObj.map((restaurantcard) => (
