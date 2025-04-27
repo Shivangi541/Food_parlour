9494374e63371db6089d2a9b98f699f317e3f5dd
Shivangi541 <shivangidutta68@gmail.com>
2025-04-28 00:33:14 +0530

    added children routes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ import REACT from "react";
 import REACTDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Cart from "./components/Cart";
 import Error from "./components/Error";
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+
+      <Outlet />
     </div>
   );
 };
@@ -18,16 +20,24 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
     errorElement: <Error />,
   },
 ]);
